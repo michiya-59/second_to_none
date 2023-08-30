361e6ad9e6d6766b8d05dc2a2b90db5084686174
michiya-59 <nishino.michiya0509@gmail.com>
2023-08-30 09:04:42 +0900

    Time.zoneの変更とRUbocopの変更

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module ShineStnApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.time_zone = "Asia/Tokyo"
+    config.time_zone = "Tokyo"
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join("config/locales/**/*.yml").to_s]
