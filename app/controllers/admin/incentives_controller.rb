bc5115287ea396b30466925016f1ccdcb6a95d5d
michiya-59 <nishino.michiya0509@gmail.com>
2023-11-12 23:05:36 +0900

    報酬情報画面の完成

--- app/controllers/admin/incentives_controller.rb
+++ app/controllers/admin/incentives_controller.rb
@@ -2,5 +2,28 @@
 
 module Admin
   class IncentivesController < ApplicationController
+    def index
+      @incentives = Incentive.order(incentive_price: :desc)
+    end
+
+    def edit
+      @incentive = Incentive.find(params[:id])
+    end
+
+    def update
+      @incentive = Incentive.find(params[:id])
+      if @incentive.update(incentive_params)
+        flash[:success] = "報酬情報の更新が完了しました。"
+        redirect_to admin_incentives_path
+      else
+        render :edit
+      end
+    end
+
+    private 
+    
+    def incentive_params
+      params.require(:incentive).permit(:course_name, :incentive_price)
+    end
   end
 end
--- app/models/incentive.rb
+++ app/models/incentive.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class Incentive < ApplicationRecord
+  validates :course_name, presence: true
+  validates :incentive_price, presence: true, numericality: {only_integer: true}
 end
