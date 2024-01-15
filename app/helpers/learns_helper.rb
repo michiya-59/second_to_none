390407d4a52f6686a039a61c8fa697ec004083eb
michiya-59 <nishino.michiya0509@gmail.com>
2024-01-15 20:39:37 +0900

    新規MTの表示修正

--- app/helpers/learns_helper.rb
+++ app/helpers/learns_helper.rb
@@ -8,11 +8,15 @@ module LearnsHelper
   #   current_user.video_views.where(learn_id: required_views).count == required_views.count
   # end
 
-  def task_submitted? learn_id, user_id
+  def task_submitted? learn_id, user_id, index
     # 最初の3つの資料には制限をかけない
-    return true if learn_id <= 3
+    return true if index <= 3
 
-    # 4つ目以降の学びの資料については、前の学びの資料の課題が提出されているかを確認
-    (1..learn_id - 1).all?{|id| Task.exists?(learn_id: id, user_id:)}
+    # 4つ目以降の資料については、それまでの全ての資料の課題が提出されているかを確認
+    # そのために、現在のlearn_idより小さい全てのlearn_idを取得
+    previous_learn_ids = Learn.where("id < ?", learn_id).pluck(:id)
+
+    # それらのlearn_idに対して、全ての課題が提出されているかを確認
+    previous_learn_ids.all?{|id| Task.exists?(learn_id: id, user_id:)}
   end
 end
