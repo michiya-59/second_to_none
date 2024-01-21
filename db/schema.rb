3a4b9ad7ac23614e25c08216f210960a62f4a8e5
michiya-59 <nishino.michiya0509@gmail.com>
2024-01-21 20:14:50 +0900

    学びの資料のカテゴリーを自由に並び替えるよう修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_20_063301) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_21_105823) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -94,6 +94,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_20_063301) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "order_num"
   end
 
   create_table "learns", force: :cascade do |t|
